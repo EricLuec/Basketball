45a3a48bbf868f4c5c75aa2538271b1eb269b727
ericluec <eric.luecking@gmail.com>
2024-03-19 15:39:18 +0100

    workin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Team from './components/team';
 import History from './components/history';
+import Intro from './components/Intro';
 
 const App = () => {
   return (
     <div className="container">
-      
+      <Intro />
       <Team className="tweak"/>
       <History className="tweak" />
       <Team className="tweak"/>
--- frontend/src/components/Intro.jsx
+++ frontend/src/components/Intro.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import './MyComponent.css'; // Stil-Datei für CSS
-import icon from 'public/euler.jpg'; // Importiere das Icon-Bild
 
-function MyComponent() {
+function Intro() {
   return (
     <div className="my-component">
-      <img src={icon} alt="Icon" className="icon" />
+      <img src="/public/euler.jpg" alt="Icon" className="icon" />
       <h1 className="title">Fetter Text</h1>
       <p className="description">Ein kleiner Text</p>
     </div>
   );
 }
 
-export default MyComponent;
+export default Intro;
