5f9efeaae4e907124f005158ee7d1efa3af9ce90
ericluec <eric.luecking@gmail.com>
2024-03-20 09:51:13 +0100

    workin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Team from './components/team';
 import History from './components/history';
-import Intro from './components/Intro';
 
 const App = () => {
   return (
     <div className="container">
-      <Intro />
       <Team className="tweak"/>
       <History className="tweak" />
       <Team className="tweak"/>
